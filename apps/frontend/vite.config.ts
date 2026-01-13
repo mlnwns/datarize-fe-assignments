d9fdda4d78a656b5b2396bb32886b4786f981e76
mlnwns <mlnwns@naver.com>
2026-01-14 02:35:31 +0900

    test: 테스트 환경설정

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	// @ts-expect-error vitejs/plugin-react 타입 문제
 	plugins: [react()],
 	server: {
 		port: 3000,
@@ -17,6 +18,21 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: ["./src/test/setup.ts"],
+		coverage: {
+			reporter: ["text", "json", "html"],
+			exclude: [
+				"node_modules/",
+				"src/test/",
+				"**/*.styled.ts",
+				"**/*.config.ts",
+				"src/main.tsx",
+			],
 		},
 	},
 });

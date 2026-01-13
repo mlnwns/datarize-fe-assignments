4c1450d7a883461b799270352658d4b126fcaf02
mlnwns <mlnwns@naver.com>
2026-01-13 13:16:05 +0900

    feat: 고객 관련 타입 정의

---
+++ apps/frontend/src/apis/customer/type.ts
@@ -0,0 +1,35 @@
+import type { DateRangeParams, ISODateString } from "@/shared/types/date";
+
+export interface Customer {
+	id: number;
+	name: string;
+	count: number;
+	totalAmount: number;
+}
+
+export interface Pagination {
+	limit: number;
+	page: number;
+	total: number;
+	totalPages: number;
+}
+
+export interface CustomerListResponse {
+	data: Customer[];
+	pagination: Pagination;
+}
+
+export type CustomerListParams = DateRangeParams & {
+	sortBy?: "asc" | "desc";
+	name?: string;
+	page?: number;
+	limit?: number;
+};
+
+export interface CustomerPurchaseDetail {
+	date: ISODateString;
+	quantity: number;
+	product: string;
+	price: number;
+	imgSrc: string;
+}
---
+++ apps/frontend/src/shared/types/date.ts
@@ -0,0 +1,11 @@
+/**
+ * ISO 8601 date string
+ * format: YYYY-MM-DD
+ */
+
+export type ISODateString = string;
+
+export type DateRangeParams = {
+	from?: ISODateString | null;
+	to?: ISODateString | null;
+};

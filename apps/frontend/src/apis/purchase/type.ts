925969f7e244ff15f4b0c7253a411808603356fe
mlnwns <mlnwns@naver.com>
2026-01-13 13:16:46 +0900

    feat: 구매 관련 타입 정의

---
+++ apps/frontend/src/apis/purchase/type.ts
@@ -0,0 +1,12 @@
+export interface PurchaseFrequency {
+	range: string;
+	count: number;
+}
+
+export interface Purchase {
+	date: string;
+	customerName: string;
+	productName: string;
+	price: number;
+	quantity: number;
+}
